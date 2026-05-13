0d9cb0aa825ad60ed526fa6a74be46074c07eec1
Filipa Lima <filipalima1995@gmail.com>
2026-05-13 16:24:45 +0100

    feat: create project structure and their dependencies

---
+++ playwright.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from '@playwright/test';
+import * as dotenv from 'dotenv';
+
+export default defineConfig({
+    testDir: './tests/features',
+    use: {
+        baseURL: process.env.BASE_URL
+    },
+    reporter: 'html',
+});
