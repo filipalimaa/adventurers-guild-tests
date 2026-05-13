0eb2675b72c16053de6a1980cc367e0267d43d01
Filipa Lima <filipalima1995@gmail.com>
2026-05-13 22:14:36 +0100

    feat: create interface types for character creation API request and response

---
+++ tests/types/characterRequest.ts
@@ -0,0 +1,15 @@
+export interface CharacterRequestPost {
+    name: string,
+};
+
+export interface CharacterRequestClassPatch {
+    classId: number,
+};
+
+export interface CharacterRequestSpeciesPatch {
+    speciesId: number,
+};
+
+export interface CharacterRequestBackgroundPatch {
+    backgroundId: number,
+};
